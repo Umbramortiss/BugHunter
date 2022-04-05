3d18afe6e5f40259eacd17746ad686de16fb203e
Umbramortiss <55592845+Umbramortiss@users.noreply.github.com>
2022-04-05 00:51:12 -0400

    update bugsub.sh

--- bugsub.sh
+++ bugsub.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #colors
-red='tput setaf 1'
-green='tput setaf 2'
-yellow='tput setaf 3'
-cyan='tput setaf 6'
-magenta='tput setaf 5'
-reset='tput sgr0'
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+cyan=`tput setaf 6`
+magenta=`tput setaf 5`
+reset=`tput sgr0`
 
 read -p "Enter The Domain Name: " DOM 
 
