1777bf5733d2ad5ef47afd5fba56c0b35e164112
Umbramortiss <55592845+Umbramortiss@users.noreply.github.com>
2022-04-05 23:56:59 -0400

    update bugxss.sh

--- bugxss.sh
+++ bugxss.sh
@@ -7,3 +7,31 @@ yellow=`tput setaf 3`
 cyan=`tput setaf 6`
 magenta=`tput setaf 5`
 white=`tput setaf 7`
+reset=`tput sgr0`
+
+
+read -p "Enter The Domain Name: " Domain
+
+if [ -d ~/Bughunt]
+then
+    echo " "
+else
+    mkdir ~/Bughunt
+fi
+
+
+if [ -d ~/Bughunt/$DOM ]
+then
+    echo " "
+else
+    mkdir ~/Bughunt/$DOM
+fi
+
+if [ -d ~/Bughunt/$DOM/Subdomains ]
+then
+    echo " "
+else
+mkdir ~/Bughunt/$DOM/Subdomains
+fi
+
+echo "${cyan} [+] Fetching URLS ${reset}"
