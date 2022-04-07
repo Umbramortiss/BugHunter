e237e601da8b97acc827cd7d9bcc5faaa2ddd683
Umbramortiss <55592845+Umbramortiss@users.noreply.github.com>
2022-04-07 14:52:23 -0400

    update bugxss.sh

--- bugxss.sh
+++ bugxss.sh
@@ -12,26 +12,26 @@ reset=`tput sgr0`
 
 read -p "Enter The Domain Name: " DOM
 
-if [ -d ~/Bughunt]
+if [ -d ~/Bughunt/Bugxss ]
 then
     echo " "
 else
-    mkdir ~/Bughunt
+    mkdir ~/Bughunt/Bugxss
 fi
 
 
-if [ -d ~/Bughunt/$DOM ]
+if [ -d ~/Bughunt/Bugxss/$DOM ]
 then
     echo " "
 else
-    mkdir ~/Bughunt/$DOM
+    mkdir ~/Bughunt/Bugxss/$DOM
 fi
 
-if [ -d ~/Bughunt/$DOM/XSS ]
+if [ -d ~/Bughunt/Bugxss/$DOM/XSS ]
 then
     echo " "
 else
-mkdir ~/Bughunt/$DOM/XSS
+mkdir ~/Bughunt/Bugxss/$DOM/XSS
 fi
 
 echo "${cyan} [+] Fetching URLS ${reset}"
@@ -44,12 +44,12 @@ echo " "
 if [ -f /usr/bin/gau ]
 then 
     echo "${magenta} [+] Running Gau for retriving URLs ${reset}"
-    gau --fc 404 --subs $DOM >> ~/Bughunt/$DOM/XSS/${DOM}-urls.txt
+    gau --fc 404 --subs $DOM >> ~/Bughunt/Bugxss/$DOM/XSS/${DOM}-urls.txt
 else
     echo "${cyan} [+] Installing Gau ${reset}"
     go g
     echo "${magenta} [+] Running Gau for retriving URLs ${reset}"
-    gau --fc 404 --subs $DOM >> ~/Bughunt/$DOM/XSS/${DOM}-urls.txt
+    gau --fc 404 --subs $DOM >> ~/Bughunt/Bugxss/$DOM/XSS/${DOM}-urls.txt
 fi
 echo " "
 echo "${white} [+] Successfully saved as ${DOM}-url.txt ${reset}"
