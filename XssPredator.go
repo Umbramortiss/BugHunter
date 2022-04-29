4ca2aa8c347dc5f6e53599702a260e9b96f44900
Umbramortiss <55592845+Umbramortiss@users.noreply.github.com>
2022-04-29 16:38:29 -0400

    update XssPredator.go

--- XssPredator.go
+++ XssPredator.go
@@ -1,74 +1,77 @@
-
 package main
 
 import(
- "fmt"
- "os/exec"
- "runtime"
+    "fmt"
+    "os/exec"
+    "runtime"
 )
 
 var domain string
 
-func dirCheck(){
-    if _, err := os.Stat("~/Bughunt/Bugxss"); os.IsNotExist(err){
+func dirCheck() {
+    if _,
+    err: = os.Stat("~/Bughunt/Bugxss"); os.IsNotExist(err) {
         os.Mkdir("~/Bughunt/Bugxss", 0755)
-        
+
         if err != nil {
             fmt.Printf("%s", err)
         }
         fmt.Print("Your vuln den was created")
-        
+
     }
 
-    if _, err := os.Stat("~/Bughunt/Bugxss/domain"); os.IsNotExist(err){
+    if _,
+    err: = os.Stat("~/Bughunt/Bugxss/domain"); os.IsNotExist(err) {
         os.Mkdir("~/Bughunt/Bugxss/domain", 0755)
-            if err != nil {
-                 fmt.Printf("%s", err)
-            }
-        
+        if err != nil {
+            fmt.Printf("%s", err)
+        }
+
     }
-    if _, err := os.Stat("~/Bughunt/Bugxss/domain/xss"); os.IsNotExist(err){
+    if _,
+    err: = os.Stat("~/Bughunt/Bugxss/domain/xss"); os.IsNotExist(err) {
         os.Mkdir("~/Bughunt/Bugxss/domain/xss", 0755)
-            if err != nil {
-                fmt.Printf("%s", err)
-            }
-        
+        if err != nil {
+            fmt.Printf("%s", err)
+        }
+
     }
-    
+
 }
 
-func execute(){
+func execute() {
     //
-    if _, err := os.Stat("/usr/bin/gau"); os.IsNotExist(err){
-        out, err := exec.Command("gau","--subs").Output()
-        
+    if _,
+    err: = os.Stat("/usr/bin/gau"); os.IsNotExist(err) {
+        out,
+        err: = exec.Command("gau", "--subs").Output()
+
         if err != nil {
             fmt.Printf("%s", err)
         }
         fmt.Println("Running gau for fetching URLs")
-        output := string(out[:])
+        output: = string(out[:])
         fmt.Println(output)
-        
+
     }
-    
-    
-    
+
+
+
 }
 
 func main() {
-    
+
     if runtime.GOOS == "windows" {
         fmt.Println("Can't execute this on a windows machine")
     } else {
-    fmt.Print("Enter your domain: ")
-    fmt.Scanf("%s" &domain)
-    dirCheck()
-    execute()
-        
+        fmt.Print("Enter your domain: ")
+        fmt.Scanf("%s" &domain)
+        dirCheck()
+        execute()
+
     }
-    
-    
-    
+
+
+
 
 }
-
