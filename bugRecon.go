016876626a6989cfc2ab4250d346fb4fdc0c8728
UmbraMortis <you@example.com>
2024-02-29 16:07:34 -0500

    update

---
+++ bugRecon.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"sync"
+	"io/ioutil"
+)
+
+var domain string
+var output []string
+var liveOutput []string
+
+
+func domName() {
+	fmt.Print("Enter your domain: ")
+	fmt.Scanf("%s", &domain)
+}
+
+``
+
+func gau(wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	if _, err := os.Stat("/usr/bin/gau"); err == nil {
+
+		out, err := exec.Command("gau", "--subs", domain).Output()
+		fmt.Println(out)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println("Running gau for fetching URLs")
+		output = append(output, string(out[:]))
+		fmt.Println(output)
+
+	}else if os.IsNotExist(err) {
+		fmt.Println("gau command not found")
+	}
+
+}
+
+func httpx(wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	if _,
+		err := os.Stat("/usr/bin/httpx"); err == nil {
+			file, err := ioutil.TempFile("","httpx")
+			if err != nil {
+				fmt.Printf("Error creating temp file: %s", err)
+				return
+			}
+			defer os.Remove(file.Name())
+
+			if _, err := file.WriteString(strings.Join(output, "\n")); err != nil {
+				fmt.Printf("Error writing to temp file: %s", err)
+				return
+			}
+		out,
+			err := exec.Command("httpx", "-l",file.Name(), "--no-color", domain).Output()
+		fmt.Println(err)
+		if err != nil {
+			fmt.Printf("%s", err)
+
+		}
+		fmt.Println("Running httpx for live URLs")
+		liveOutput := string(out[:])
+		fmt.Println(liveOutput)
+
+	}else if os.IsNotExist(err) {
+		fmt.Println("gau command not found")
+	}
+
+}
+func assetf(wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	if _,
+		err := os.Stat("/usr/bin/assetfinder"); err == nil{
+		out,
+			err := exec.Command("assetfinder", "-subs-only", domain).Output()
+
+		if err != nil {
+			fmt.Printf("Error running assetfinder: %s", err)
+		}
+		fmt.Println("Running assetfinder for domain enumeration")
+		output = append(output, string(out[:]))
+		fmt.Println(output)
+	}else if os.IsNotExist(err) {
+		fmt.Println("gau command not found")
+	}
+}
+func amassF(wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	if _,
+		err := os.Stat("/usr/bin/amass"); err == nil {
+		out,
+			err := exec.Command("amass", "enum", "--passive", "-d", domain).Output()
+
+		if err != nil {
+			fmt.Printf("%f", err)
+		}
+		fmt.Println("Running amass for domain enumeration")
+		output = append(output, string(out[:]))
+		fmt.Println(output)
+	}else if os.IsNotExist(err) {
+		fmt.Println("gau command not found")
+	}
+}
+func subF(wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	if _,
+		err := exec.LookPath("subfinder"); err == nil {
+		out,
+			err := exec.Command("subfinder", "-d", domain).Output()
+
+		if err != nil {
+			fmt.Printf("Error running subfinder: %s", err)
+		}
+		fmt.Println("Running subfinder for domain enumeration")
+		output = append(output, string(out[:]))
+		fmt.Println(output)
+	}else if os.IsNotExist(err) {
+		fmt.Println("gau command not found")
+	}
+}
+
+func subList3r(wg *sync.WaitGroup){
+	defer wg.Done()
+
+	if _, err := os.Stat("/usr/bin/sublist3r"); err == nil{
+		out, err := exec.Command("sublist3r", "-b", "-d", domain).Output()
+
+		if err != nil {
+			fmt.Printf("Error running sublist3r: %s", err)
+		}
+		fmt.Println("Running sublist3r for domain enumeration")
+		output = append(output, string(out[:]))
+		fmt.Println(output)
+	}else if os.IsNotExist(err) {
+		fmt.Println("gau command not found")
+	}
+}
+
+
+func subSort(s []string) []string {
+	inResult := make(map[string]bool)
+	var result []string
+	for _, str := range s {
+		if _, ok := inResult[str]; !ok {
+			inResult[str] = true
+			result = append(result, str)
+		}
+	}
+	return result
+} 
+
+func writeToCSV(fileName string, data []string)error {
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	w := csv.NewWriter(f)
+	defer w.Flush()
+
+	for _, file := range data {
+		if err := w.Write([]string{file});err != nil{
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	if runtime.GOOS == "windows" {
+		fmt.Println("Can't execute this on a windows machine")
+	} else {
+
+		domName()
+		wg.Add(6)
+		go gau(&wg)
+		go httpx(&wg)
+		go assetf(&wg)
+		go subF(&wg)
+		go amassF(&wg)
+		go subList3r(&wg)
+		wg.Wait()
+		fmt.Println("Finished all the task")
+		out := subSort(liveOutput)
+		fmt.Println(out)
+		csvFileName := domain + ".csv"
+		if err := writeToCSV(csvFileName, out); err != nil {
+			fmt.Println("Error writing CSV file:", err)
+		}else{
+			fmt.Println("Saved output to CSV file:",csvFileName )
+		}
+
+		fmt.Println("Done!")
+
+	}
+
+}
---
+++ subRecon.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"sync"
+)
+
+var domain string
+var output []string
+
+func domName() {
+	fmt.Print("Enter your domain: ")
+	fmt.Scanf("%s", &domain)
+}
+
+/*
+func dirCheck() {
+    if _, err := os.Stat("~/Bughunt/Bugxss"); os.IsNotExist(err) {
+        os.Mkdir("~/Bughunt/Bugxss", 0755)
+
+        if err != nil {
+            fmt.Printf("%s", err)
+        }
+        fmt.Print("Your vuln den was created")
+
+    }
+https://www.td.com/
+    if _, err := os.Stat("~/Bughunt/Bugxss/domain"); os.IsNotExist(err) {
+        os.Mkdir("~/Bughunt/Bugxss/domain", 0755)
+        if err != nil {
+            fmt.Printf("%s", err)
+        }
+
+    }
+    if _,
+    err := os.Stat("~/Bughunt/Bugxss/domain/xss"); os.IsNotExist(err) {
+        os.Mkdir("~/Bughunt/Bugxss/domain/xss", 0755)
+        if err != nil {
+            fmt.Printf("%s", err)
+        }
+
+    }
+
+}
+*/
+
+func gau(wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	if _, err := os.Stat("/usr/local/bin/gau"); os.IsNotExist(err) {
+
+		out, err := exec.Command("gau", "%s", "--subs", domain).Output()
+		fmt.Println(out)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println("Running gau for fetching URLs")
+		output := string(out[:])
+		fmt.Println(output)
+
+	}
+
+}
+
+func httpx(wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	if _,
+		err := os.Stat("/usr/local/bin/httpx"); os.IsNotExist(err) {
+		out,
+			err := exec.Command("httpx", "%s", "-silent", domain).Output()
+		fmt.Println(err)
+		if err != nil {
+			fmt.Printf("%s", err)
+
+		}
+		fmt.Println("Running httpx for live URLs")
+		liveOutput := string(out[:])
+		fmt.Println(liveOutput)
+
+	}
+
+}
+func assetf(wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	if _,
+		err := os.Stat("/usr/bin/assetfinder"); os.IsNotExist(err) {
+		out,
+			err := exec.Command("assetfinder", "-subs-only", domain).Output()
+
+		if err != nil {
+			fmt.Printf("Error running assetfinder: %s", err)
+		}
+		fmt.Println("Running assetfinder for domain enumeration")
+		output := string(out[:])
+		fmt.Println(output)
+	}
+}
+
+func subLis3r(wg *sync.WaitGroup){
+	defer wg.Done()
+
+	if _, err := os.Stat
+}
+
+func subSort(s []string) []string {
+	inResult := make(map[string]bool)
+	var result []string
+	for _, str := range s {
+		if _, ok := inResult[str]; !ok {
+			inResult[str] = true
+			result = append(result, str)
+		}
+	}
+	return result
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	if runtime.GOOS == "windows" {
+		fmt.Println("Can't execute this on a windows machine")
+	} else {
+
+		domName()
+		//dirCheck()
+		wg.Add(2)
+		go gau(&wg)
+		//go httpx(&wg)
+		go assetf(&wg)
+		//go subF(&wg)
+		//go amassF(&wg)
+		wg.Wait()
+		fmt.Println("Finished all the task")
+		out := subSort(output)
+		fmt.Println(out)
+		fmt.Println("Done!")
+
+	}
+
+}
